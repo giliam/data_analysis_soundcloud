4c4c960b17d72813025220dc1d8db634df542475
giliam <matthieu.denoux@gmail.com>
2015-11-16 23:23:39 +0100

    Updates extract to try to find the file url.

--- extract.py
+++ extract.py
@@ -2,7 +2,7 @@
 import requests
 import csv
 import json
-
+import shutil
 from secret_data import CLIENT_ID, CLIENT_SECRET
 
 class SoundCloudAnalyse:
@@ -23,10 +23,26 @@ class SoundCloudAnalyse:
         for playlist in playlists:
             tracks = self.get_tracks_from_playlist(playlist)
             for track in tracks:
+                if track['sharing'] == 'public':
+                    print(track['stream_url'])
+                else:
+                    print track['sharing']
                 comments.append(self.get_comments_from_track(track['id']))
-        return comments
+        return []
+
+    def get_stream(self, track_id):
+        r_track = requests.get("http://api.soundcloud.com/tracks/" + str(track_id) + "/stream?client_id=" + CLIENT_ID) 
+        print("http://api.soundcloud.com/tracks/" + str(track_id) + "/download?client_id=" + CLIENT_ID)
+        if r_track.status_code == 200:
+            print(r_track.raw)
+            with open("test.wav", 'wb') as f:
+                r_track.raw.decode_content = True
+                shutil.copyfileobj(r_track.raw, f)
+        else:
+            print("Unable to reach such file (" + str(track_id) + ").")
 
 
 if __name__ == "__main__":
     sca = SoundCloudAnalyse()
-    print(sca.get_comments_from_playlists(["163043603"]))
+    sca.get_stream("228081985")
+    print(sca.get_comments_from_playlists(["163424726"]))
